7b1bbc036cc587b62e866d8fc64533f97b4ec6bc
Sebastián Barrenechea <sebastian@barrenechea.cl>
2022-08-03 17:14:23 -0400

    fix: add cors for dev envs when querying a deployed api

--- Dockerfile
+++ Dockerfile
@@ -5,7 +5,8 @@ RUN npm install
 
 FROM node:18-alpine as app
 WORKDIR /app
-COPY . .
-COPY --from=installer /app/node_modules ./node_modules
+USER node
+COPY --chown=node:node . .
+COPY --from=installer --chown=node:node /app/node_modules ./node_modules
 
 CMD ["node", "index.js"]
--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 import express from 'express';
+import cors from 'cors';
 import swaggerUi from 'swagger-ui-express';
 import swaggerJSDoc from 'swagger-jsdoc';
 
 import { getMenu, getOrders, postOrder } from './routes.js';
 import { sequelize } from './db/index.js';
 
+const corsOptions = {
+  origin: ['http://localhost:5173', 'http://127.0.0.1:5173'],
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+
 const app = express();
 app.use(express.json());
+app.use(cors(corsOptions));
 
 const options = {
   definition: {
